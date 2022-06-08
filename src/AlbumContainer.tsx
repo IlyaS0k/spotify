f156f90279d2b5923146514364d08f005ed8f743
IlyaS0k <ilyasokolov63@mail.ru>
2022-06-08 20:19:14 +0300

    Проект. часть 3

---
+++ src/AlbumContainer.tsx
@@ -0,0 +1,21 @@
+import {AlbumList} from './AlbumList'
+import {Album} from './Album'
+import React, { useState, useEffect } from 'react'
+import { AlbumCard } from './AlbumCard';
+
+export function AlbumContainer(props:{albumTitle: string, albumList: Album[], promise: Promise<AlbumList>})
+{ 
+  const [albums, setAlbums] = useState(props.albumList);
+  const [tag, setTag] = useState(props.albumTitle);
+  props.promise.then((data)=>{
+  setAlbums(data.getList());
+  setTag(data.getTitle());
+  })
+  
+  return <div>
+   <div className='main-field-title'>{tag}</div>
+   <div className='big_item'>
+   {albums.slice(0,5).map(data => <AlbumCard album={data}/>)}
+   </div>
+  </div> 
+}
